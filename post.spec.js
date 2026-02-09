96ef54e1afaaa5319d29d4574a122c2ccfbfcdf8
harinie-4466 <sviharinie@gmail.com>
2026-02-09 15:49:27 +0530

    Functional tests

--- post.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('TC-CONT-201 Create post', async ({ page }) => {
-  await page.goto('/home');
-  await page.fill('#post-box', 'Hello World');
-  await page.click('#submit-post');
-  await expect(page.locator('.post')).toContainText('Hello World');
-});
